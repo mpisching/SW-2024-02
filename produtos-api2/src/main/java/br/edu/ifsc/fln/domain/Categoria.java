2db9f74a176d8c9478f88010f7c0d1bd6f51b327
mpisching <55263037+mpisching@users.noreply.github.com>
2024-11-06 17:51:00 -0300

    CRUD Completo Categoria

--- produtos-api2/src/main/java/br/edu/ifsc/fln/domain/Categoria.java
+++ produtos-api2/src/main/java/br/edu/ifsc/fln/domain/Categoria.java
@@ -23,33 +23,5 @@ public class Categoria {
 	private int id;
 	
 	private String descricao;
-//	public int getId() {
-//		return id;
-//	}
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//	public String getDescricao() {
-//		return descricao;
-//	}
-//	public void setDescricao(String descricao) {
-//		this.descricao = descricao;
-//	}
-//	@Override
-//	public int hashCode() {
-//		return Objects.hash(id);
-//	}
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		Categoria other = (Categoria) obj;
-//		return id == other.id;
-//	}
-//	
 	
 }
--- produtos-api2/src/main/java/br/edu/ifsc/fln/repository/CategoriaRepository.java
+++ produtos-api2/src/main/java/br/edu/ifsc/fln/repository/CategoriaRepository.java
@@ -1,5 +1,7 @@
 package br.edu.ifsc.fln.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import br.edu.ifsc.fln.domain.Categoria;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
-
+	//https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+	List<Categoria> findByDescricaoContaining(String descricao);
+	List<Categoria> findByDescricao(String descricao);
 }
